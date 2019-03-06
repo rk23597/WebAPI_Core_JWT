238bf23da306fa365dcf45b5ac48a1643d76aebe
rkdhacker <rkdhacker.in@gmail.com>
2019-03-06 15:59:04 +0530

    initial code

---
+++ WebAPI_Core_JWT/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPI_Core_JWT.Controllers
+{
+    public class TokenController : Controller
+    {
+        private const string SECRET_KEY = "TQvgjeABMPOwCycOqah5EQu5yyVjpmVG";
+        public static readonly SymmetricSecurityKey SIGNING_KEY = new
+                      SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
+
+        [HttpGet]
+        [Route("api/Token/{username}/{password}")]
+        public IActionResult Get(string username, string password)
+        {
+            if (username == password)
+                return new ObjectResult(GenerateToken(username));
+            else
+                return BadRequest();
+        }
+
+        // Generate a Token with expiration date and Claim meta-data.
+        // And sign the token with the SIGNING_KEY
+        private string GenerateToken(string username)
+        {
+            var token = new JwtSecurityToken(
+                claims: new Claim[] { new Claim(ClaimTypes.Name, username) },
+                notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                expires: new DateTimeOffset(DateTime.Now.AddMinutes(60)).DateTime,
+                signingCredentials: new SigningCredentials(SIGNING_KEY,
+                                                    SecurityAlgorithms.HmacSha256)
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ WebAPI_Core_JWT/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebAPI_Core_JWT.Controllers;
+
+namespace WebAPI_Core_JWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+
+            }).AddJwtBearer("JwtBearer", jwtOptions => {
+
+                jwtOptions.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    IssuerSigningKey = TokenController.SIGNING_KEY,
+                    ValidateIssuer=false,
+                    ValidateAudience=false,
+                    ValidateIssuerSigningKey=true,
+                    ValidateLifetime=true,
+                    ClockSkew=TimeSpan.FromMinutes(5)
+
+                };
+
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            //must be used before app.useMvc if wants to assign token to authorzie attributes
+            app.UseAuthentication();
+            
+            app.UseMvc();
+
+           
+        }
+    }
+}
